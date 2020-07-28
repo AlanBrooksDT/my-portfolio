b8a3123fe3fc99f0a082843d5985088a7162ce1f
Alan Brooks <abrooks@live.co.uk>
2020-07-29 00:01:20 +0100

    responsive form success!

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-document.getElementById("submit").addEventListener("click", function(){
+document.getElementById("btnsubmit").addEventListener("click", function(){
     document.getElementById("confirmSent").src='sent.gif'; 
-    event.preventDefault();
+    /*event.preventDefault();*/
     var nameVal = document.getElementById("name");
     var email = document.getElementById("mail");
     var textArea = document.getElementById("msg");
     window.alert("Name: " + nameVal.value + "\r\n\r\n" + "E-mail: " + email.value + "\r\n\r\n" + "Message: " + textArea.value);
     });
 
-document.getElementById("reset").addEventListener("click", function(){
+document.getElementById("btnreset").addEventListener("click", function(){
     window.location.reload();});
+
+document.getElementById("name").addEventListener("click", function(){
+    document.getElementById("name").value="";
+    })
+
+document.getElementById("mail").addEventListener("click", function(){
+    document.getElementById("mail").value="";
+    })
